368de37067fcbb4917499b3d987722f63f48c13f
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-03-30 15:06:11 +0200

    Delete NewFile activity

--- app/src/main/java/com/rousseau_alexandre/feedid3/MainActivity.java
+++ app/src/main/java/com/rousseau_alexandre/feedid3/MainActivity.java
@@ -1,16 +1,23 @@
 package com.rousseau_alexandre.feedid3;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int READ_REQUEST_CODE = 42;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +31,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent myIntent = new Intent(MainActivity.this, NewFileActivity.class);
-                // myIntent.putExtra("key", value); //Optional parameters
-                MainActivity.this.startActivity(myIntent);
+                // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file browser.
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                // Filter to show only images, using the image MIME data type. If one wanted to
+                // search for ogg vorbis files, the type would be "audio/ogg". To search for all
+                // documents available via installed storage providers, it would be "*/*".
+                intent.setType("audio/mpeg");
+                // Filter to only show results that can be "opened", such as a file (as opposed to a
+                // list of contacts or timezones)
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
 
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                try {
+                    startActivityForResult(intent, READ_REQUEST_CODE);
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Snackbar.make(view, "Please install a File Manager.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
     }
@@ -56,4 +72,26 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i("az", "Uri: " + uri.getPath());
+            }
+        }
+    }
+
+
 }
