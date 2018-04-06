8cf492958fc31fbb201e9eac7aee7dd3b0a9d1da
Rousseau Alexandre <contact@rousseau-alexandre.fr>
2018-04-06 09:56:47 +0200

    Pass ImportedFile to intent

--- app/src/main/java/com/rousseau_alexandre/feedid3/activities/ImportedFileActivity.java
+++ app/src/main/java/com/rousseau_alexandre/feedid3/activities/ImportedFileActivity.java
@@ -3,12 +3,12 @@ package com.rousseau_alexandre.feedid3.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.rousseau_alexandre.feedid3.R;
+import com.rousseau_alexandre.feedid3.models.ImportedFile;
 
 public class ImportedFileActivity extends AppCompatActivity {
 
@@ -17,6 +17,11 @@ public class ImportedFileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imported_mp3_file);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Intent intent = getIntent();
+        ImportedFile file = (ImportedFile) intent.getSerializableExtra(MainActivity.EXTRA_MESSAGE);
+        toolbar.setTitle(file.getPath());
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
